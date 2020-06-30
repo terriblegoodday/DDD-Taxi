04ce127934ed89e00e646cc689f518648ea6dc2e
Eduard <terriblegoodday@icloud.com>
2020-06-30 21:02:48 +1000

    Added unit test target

---
+++ DDD-Taxi-UnitTests/DDD_Taxi_UnitTests.m
@@ -0,0 +1,37 @@
+//
+//  DDD_Taxi_UnitTests.m
+//  DDD-Taxi-UnitTests
+//
+//  Created by Eduard Dzhumagaliev on 6/30/20.
+//  Copyright © 2020 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DDD_Taxi_UnitTests : XCTestCase
+
+@end
+
+@implementation DDD_Taxi_UnitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
